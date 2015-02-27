655a216b61ba8b70f683907616b2bd2dce23efba
YannisRIC <yannis381@gmail.com>
2015-02-27 09:50:56 +0100

    Add temperatureRegulation process

---
+++ Trunk/Process/Process_TemperatureRegulation/Process_TemperatureRegulation.py
@@ -0,0 +1,46 @@
+# To add a new process, follow instructions :
+# Copy/Paste/rename Process_template folder
+# Go to Trunk/Core/Queue_Global.py and follow instructions
+# Go to Trunk/main.py and follow instructions
+# Rename Process name in StartThread function
+
+
+from Queue import Queue
+from threading import Thread
+
+import Core.Queue_Global as Queue_Global
+from Core.QueueItem import QueueItem
+from Core.pref import preferences
+
+def process(Queue):
+
+	while True:
+		Item = Queue.get()
+		state = Item.state
+		data = Item.data
+		if state == "Init":
+			print("Init Temperature Regulation")
+
+		elif state == "Start":
+			print("Start State")	
+
+		elif state == "Process":
+			print("Process State")		
+			Queue.enqueueIfEmpty(state, data, 1000)
+			
+		elif state == "Stop":
+			print("Stop State")		
+
+		elif state == "Exit":
+
+			break
+		else:
+			print("Programmation error : This state is not implemented : "+state)
+
+		Queue.task_done() # Indicate that a formerly enqueued task is complete
+
+
+def StartThread():
+	ThreadProcess = Thread(target=process, args=(Queue_Global.process_TemperatureRegulation,))
+	ThreadProcess.setDaemon(False)
+	ThreadProcess.start()
--- Trunk/main.py
+++ Trunk/main.py
@@ -15,6 +15,7 @@ import Core.Queue_Global as Queue_Global
 from Core.QueueItem import QueueItem
 import Process.Process_UserCommand.Process_UserCommand as Process_UserCommand
 import Process.Process_SensorsRead.Process_SensorsRead as Process_SensorsRead
+import Process.Process_TemperatureRegulation.Process_TemperatureRegulation as Process_TemperatureRegulation
 
 
 
@@ -23,6 +24,8 @@ import Process.Process_SensorsRead.Process_SensorsRead as Process_SensorsRead
 Process_UserCommand.StartThread()
 Process_SensorsRead.StartThread()
 Process_Actuators.StartThread()
+Process_Actuators.StartThread()
+Process_TemperatureRegulation.StartThread()
 
 commande = ""
 
@@ -30,6 +33,7 @@ Queue_Global.process_UserCommand.enqueue('Process')
 Queue_Global.process_SensorsRead.enqueue('Init')
 Queue_Global.process_SensorsRead.enqueue('Process')
 Queue_Global.process_Actuators.enqueue('Init')
+Queue_Global.TemperatureRegulation.enqueue('Init')
 
 while commande != "Exit":
     commande = raw_input("commande : ")
@@ -37,5 +41,6 @@ while commande != "Exit":
         Queue_Global.process_UserCommand.enqueue('Exit')
         Queue_Global.process_SensorsRead.enqueue('Exit')
         Queue_Global.process_Actuators.enqueue('Exit')
+        Queue_Global.TemperatureRegulation.enqueue('Exit')
 
 
