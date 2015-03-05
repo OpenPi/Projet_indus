e812aed53228c5455963bb5d75cd4511043d5992
yannis RICHARD <yannis381@gmail.com>
2015-03-05 09:43:39 +0000

    Export database

--- Ressources/Database/database.sql
+++ Ressources/Database/database.sql
@@ -1,144 +1,132 @@
 -- phpMyAdmin SQL Dump
--- version 4.2.5
+-- version 3.4.11.1deb2+deb7u1
 -- http://www.phpmyadmin.net
 --
--- Client :  localhost:8889
--- Généré le :  Mar 24 Février 2015 à 13:17
--- Version du serveur :  5.5.38
--- Version de PHP :  5.5.14
+-- Host: localhost
+-- Generation Time: Mar 05, 2015 at 09:42 AM
+-- Server version: 5.5.41
+-- PHP Version: 5.4.36-0+deb7u3
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
 --
--- Base de données :  `PlashBoard`
+-- Database: `PlashBoard`
 --
-CREATE DATABASE IF NOT EXISTS `PlashBoard` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+CREATE DATABASE `PlashBoard` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
 USE `PlashBoard`;
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `hardwareConfiguration`
+-- Table structure for table `hardwareConfiguration`
 --
 
-CREATE TABLE `hardwareConfiguration` (
-`id` int(11) NOT NULL,
-  `hardwareName` varchar(255) NOT NULL,
+CREATE TABLE IF NOT EXISTS `hardwareConfiguration` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
   `pin` tinyint(2) NOT NULL,
   `type` enum('Analog','Numeric','','') NOT NULL,
   `IO` enum('Input','Output','','') NOT NULL,
   `PullUpDownResistor` enum('None','Pull-Up','Pull-Down','') NOT NULL,
   `unit` varchar(50) DEFAULT NULL,
-  `tension` float DEFAULT NULL
+  `tension` float DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
+
+--
+-- Dumping data for table `hardwareConfiguration`
+--
+
+INSERT INTO `hardwareConfiguration` (`id`, `name`, `pin`, `type`, `IO`, `PullUpDownResistor`, `unit`, `tension`) VALUES
+(1, 'Pool Temperature Sensor', 1, 'Analog', 'Input', 'None', '°c', NULL),
+(2, 'Pool Light', 1, 'Numeric', 'Output', 'None', NULL, 5),
+(3, 'Pump Ampere Meter', 3, 'Analog', 'Input', 'None', 'A', NULL),
+(4, 'Pool Ph Meter', 4, 'Analog', 'Input', 'None', NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `measure`
+--
+
+CREATE TABLE IF NOT EXISTS `measure` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `hardwareConfigurationId` int(11) NOT NULL,
+  `value` float NOT NULL,
+  `timestamp` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `hardwareConfiguration` (`hardwareConfigurationId`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=295470 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `userCommand`
+--
+
+CREATE TABLE IF NOT EXISTS `userCommand` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `type` enum('UserCommand','Actuators') NOT NULL,
+  `command` enum('Create','Update','Delete','Get','Set') NOT NULL,
+  `targetName` varchar(255) NOT NULL,
+  `value` varchar(255) NOT NULL,
+  `timestamp` datetime NOT NULL,
+  `done` tinyint(1) NOT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `measure`
+-- Table structure for table `userConfiguration`
 --
 
-CREATE TABLE `measure` (
-`id` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `userConfiguration` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
   `hardwareConfigurationId` int(11) NOT NULL,
-  `value` float NOT NULL,
-  `timestamp` datetime NOT NULL
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
-
--- --------------------------------------------------------
-
---
--- Structure de la table `userCommand`
---
-
-CREATE TABLE `userCommand` (
-`id` int(11) NOT NULL,
-  `command` enum('Create','Update','Delete','Get','Set') NOT NULL,
-  `targetName` varchar(255) NOT NULL,
+  `unit` varchar(50) NOT NULL,
   `value` varchar(255) NOT NULL,
-  `timestamp` datetime NOT NULL,
-  `done` tinyint(1) NOT NULL
+  PRIMARY KEY (`id`),
+  KEY `hardwareConfiguration` (`hardwareConfigurationId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `userConfiguration`
+-- Table structure for table `users`
 --
 
-CREATE TABLE `userConfiguration` (
-`id` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL,
-  `hardwareConfigurationId` int(11) NOT NULL,
-  `unit` varchar(50) NOT NULL,
-  `value` varchar(255) NOT NULL
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+  `password` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 --
--- Index pour les tables exportées
+-- Constraints for dumped tables
 --
 
 --
--- Index pour la table `hardwareConfiguration`
---
-ALTER TABLE `hardwareConfiguration`
- ADD PRIMARY KEY (`id`);
-
---
--- Index pour la table `measure`
+-- Constraints for table `measure`
 --
 ALTER TABLE `measure`
- ADD PRIMARY KEY (`id`), ADD KEY `hardwareConfiguration` (`hardwareConfigurationId`);
+  ADD CONSTRAINT `hardwareConfiguration` FOREIGN KEY (`hardwareConfigurationId`) REFERENCES `hardwareConfiguration` (`id`);
 
 --
--- Index pour la table `userCommand`
---
-ALTER TABLE `userCommand`
- ADD PRIMARY KEY (`id`);
-
---
--- Index pour la table `userConfiguration`
+-- Constraints for table `userConfiguration`
 --
 ALTER TABLE `userConfiguration`
- ADD PRIMARY KEY (`id`), ADD KEY `hardwareConfiguration` (`hardwareConfigurationId`);
+  ADD CONSTRAINT `hardwareConfigurationId` FOREIGN KEY (`hardwareConfigurationId`) REFERENCES `hardwareConfiguration` (`id`);
 
---
--- AUTO_INCREMENT pour les tables exportées
---
-
---
--- AUTO_INCREMENT pour la table `hardwareConfiguration`
---
-ALTER TABLE `hardwareConfiguration`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
---
--- AUTO_INCREMENT pour la table `measure`
---
-ALTER TABLE `measure`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
---
--- AUTO_INCREMENT pour la table `userCommand`
---
-ALTER TABLE `userCommand`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT pour la table `userConfiguration`
---
-ALTER TABLE `userConfiguration`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
---
--- Contraintes pour les tables exportées
---
-
---
--- Contraintes pour la table `measure`
---
-ALTER TABLE `measure`
-ADD CONSTRAINT `hardwareConfiguration` FOREIGN KEY (`hardwareConfigurationId`) REFERENCES `hardwareConfiguration` (`id`);
-
---
--- Contraintes pour la table `userConfiguration`
---
-ALTER TABLE `userConfiguration`
-ADD CONSTRAINT `hardwareConfigurationId` FOREIGN KEY (`hardwareConfigurationId`) REFERENCES `hardwareConfiguration` (`id`);
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
