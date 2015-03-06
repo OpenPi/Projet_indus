fdca3c53410a1f48b0e7f0788729e1b51904d8b8
cedricdelhaes <delhaes.c@gmail.com>
2015-03-06 16:07:26 +0100

    fix bug php and code regression with type database insertion

--- Trunk/www/usercommands/post.php
+++ Trunk/www/usercommands/post.php
@@ -1,13 +1,14 @@
-<?
+<?php
     require_once("../orm/UserCommand.php");
 
     //Retrieve POST information
+    $type        = isset ($_POST["type"]) ? $_POST["type"] : "undefined";
     $command     = isset ($_POST["command"]) ? $_POST["command"] : "undefined";
     $targetName  = isset ($_POST["targetName"]) ? $_POST["targetName"] : "undefined";
-    $value       = isset ($_POST["value"]) ? $_POST["value"] : "undefined";
+    $value       = isset ($_POST["value"]) ? $_POST["value"] : "undefined";  
 
     //Check validity
-    if( $command == "undefined" OR $targetName == "undefined" OR $value == "undefined"){
+    if( $type == "undefined" OR $command == "undefined" OR $targetName == "undefined" OR $value == "undefined"){
         $output["code"] = 400;
         $output["message"] = "Bad parameters";
 
@@ -18,11 +19,12 @@
         $tableObject = new UserCommand();
     
         $date = date("Y-m-d H:i:s");
-        $tableObject->insertRow("'NULL', '".$command."', '".$targetName."', '".$value."', '".$date."', 0");
+        $tableObject->insertRow("'NULL', ".$type.", ".$command.", '".$targetName."', '".$value."', '".$date."', 0");
 
         $output["code"] = 201;
         $output["message"] = "Data created";
 
         header('HTTP/1.1 201 Created');
     }
+
 ?>
