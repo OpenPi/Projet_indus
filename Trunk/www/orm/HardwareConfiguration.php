e7ff4a9076735d3d3ddfdaff95b3c5402cf0f60f
cedricdelhaes <delhaes.c@gmail.com>
2015-03-12 17:46:12 +0100

    forgot coma

--- Trunk/www/orm/HardwareConfiguration.php
+++ Trunk/www/orm/HardwareConfiguration.php
@@ -13,7 +13,7 @@ final class HardwareConfiguration extends Table{
                 "name"                    => "name",
                 "pin"                     => "pin",
                 "IO"                      => "IO",
-                "init"                    => "init"
+                "init"                    => "init",
                 "PullUpDownResistor"      => "PullUpDownResistor",
                 "unit"                    => "unit",
                 "tension"                 => "tension"
