bbd6d2907282935bca34bbdd2333e2de752049c9
cedricdelhaes <delhaes.c@gmail.com>
2015-06-07 12:49:24 +0200

    change get to be in RestFul mode

--- Trunk/www/measures/get.php
+++ Trunk/www/measures/get.php
@@ -5,50 +5,40 @@
     //Retrieve the optionnal param to filter response
     $xlast = isset ($_GET["xlast"]) ? $_GET["xlast"] : "undefined";
     $time  = isset ($_GET["time"]) ? $_GET["time"] : "undefined";
+    $hardware_id  = isset ($_GET["hardware"]) ? $_GET["hardware"] : "undefined";
 
     //Retrive the ID of measures required
     $id  = explode('/', $_SERVER['PATH_INFO'])[1];
 
     $tableObject = new Measure();
 
-    //If both of parameter are not given get all measures
-    if($xlast == "undefined" and $time == "undefined"){
-        //If id in url select device selected
-        if($id != ""){
-            $cond = array("hardwareConfigurationId" => array("=", $id));
-            $res = $tableObject->getRows($cond);
+    $cond = [];
+
+    if($id != ""){
+            $cond = array("id" => array("=", $id));
+    }
+    if($time != "undefined"){
+            $cond += array("timestamp" => array(">=", $time));
+    }
+    if($hardware_id != "undefined"){
+            $cond += array("hardwareConfigurationId" => array("=", $hardware_id));
+    }
+
+    if($xlast != "undefined"){
+        //If filtring on date and condition
+        if(count($cond) != 0){
+            $res = $tableObject->getRowsLimited($cond, $xlast);
+        } else {
+            $res = $tableObject->getAllLimited($xlast);
         }
-        else{
+    } else {
+        if(count($cond) != 0){
+            $res = $tableObject->getRows($cond);
+        } else {
             $res = $tableObject->getAll();
         }
     }
-    //If both parameter are given get data
-    else if($xlast != "undefined" and $time != "undefined"){
-        $cond = array("timestamp" => array(">=", $time));
-        if($id != ""){
-            $cond += array("hardwareConfigurationId" => array("=", $id));
-        }
-	   $res = $tableObject->getRowsLimited($cond, $xlast);
-    }
-    //If xlast alone
-    else if($xlast != "undefined" and $time == "undefined"){
-        if($id != ""){
-            $cond = array("hardwareConfigurationId" => array("=", $id));
-            $res = $tableObject->getRowsLimited($cond, $xlast);
-        }else{
-	       $res = $tableObject->getAllLimited($xlast);
-        }
-    }
-    //If filter by timestamp
-    else if($xlast == "undefined" and $time != "undefined"){
-        $cond = array("timestamp" => array(">=",$time));
-        //ID ressource in url
-        if($id != ""){
-            $cond += array("hardwareConfigurationId" => array("=", $id));
-        }
-	    $res = $tableObject->getRows($cond);
-    }
-    
+  
     //Insert database result in the output
     $i= 0;
     $output["measures"] = "";
